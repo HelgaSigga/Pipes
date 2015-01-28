616b49928b813bcb52876435ae321b65195e466c
Benedikt <bes28@hi.is>
2015-01-28 15:29:12 +0000

    Bætti við virkni fyrir takka
    Aðalega sjá hvort þetta virkar hjá mér

---
+++ app/Pipes/app/src/main/java/com/example/quima/pipes/Database.java
@@ -0,0 +1,16 @@
+package com.example.quima.pipes;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Benedikt Sævarss on 28.1.2015.
+ */
+public class Database extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_database);
+    }
+}
